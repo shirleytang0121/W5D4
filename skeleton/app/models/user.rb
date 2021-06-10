5917fc625567f8d8d4a10e711c0530c32e12ff57
Shirley Tang <shirleytang1996@gmail.com>
2021-06-10 18:20:28 -0400

    finish association exercise

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    has_many :registration,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :classes,
+        through: :registration,
+        source: :class
+
+    has_many :instruct,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
+
+
+
 end
